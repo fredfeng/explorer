97e9d0f70d89af1f7b0e60141177528076b83943
xwangsd <xinyuwangsd@gmail.com>
2014-04-06 20:19:40 -0500

    Isil's test case added

--- src/test/TestIntersectFSM.java
+++ src/test/TestIntersectFSM.java
@@ -12,6 +12,8 @@ public class TestIntersectFSM {
 		/** construct a simulated regular expr fsm */
 		Edge expr_edge_foo = new Edge(new refsmId("foo"));
 		Edge expr_edge_baz = new Edge(new refsmId("baz"));	
+		Edge expr_edge_bar = new Edge(new refsmId("bar"));
+		dotEdge expr_edge_dot = new dotEdge(new refsmId("dot"));
 		
 		refsmState expr_init = new refsmState(new refsmId(1), true, false);
 		refsmState expr_state_1 = new refsmState(new refsmId(2), false, false);
@@ -29,34 +31,56 @@ public class TestIntersectFSM {
 		expr.addFinalState(expr_final);
 		
 		expr_init.addOutgoingStates(expr_state_1, expr_edge_foo);
-		expr_init.addOutgoingStates(expr_state_2, expr_edge_baz);
+		expr_init.addOutgoingStates(expr_state_2, expr_edge_bar);
+		expr_init.addOutgoingStates(expr_init, expr_edge_dot);
+		expr_init.addIncomingStates(expr_init, expr_edge_dot);
 		expr_state_1.addOutgoingStates(expr_state_3, expr_edge_baz);
+		expr_state_1.addOutgoingStates(expr_state_1, expr_edge_dot);
 		expr_state_1.addIncomingStates(expr_init, expr_edge_foo);
+		expr_state_1.addIncomingStates(expr_state_1, expr_edge_dot);
 		expr_state_2.addOutgoingStates(expr_state_3, expr_edge_baz);
-		expr_state_2.addIncomingStates(expr_init, expr_edge_baz);
+		expr_state_2.addOutgoingStates(expr_state_2, expr_edge_dot);
+		expr_state_2.addIncomingStates(expr_init, expr_edge_bar);
+		expr_state_2.addIncomingStates(expr_state_2, expr_edge_dot);
 		expr_state_3.addOutgoingStates(expr_final, expr_edge_baz);
 		expr_state_3.addIncomingStates(expr_state_1, expr_edge_baz);
 		expr_state_3.addIncomingStates(expr_state_2, expr_edge_baz);
-		
+		expr_final.addOutgoingStates(expr_final, expr_edge_dot);
+		expr_final.addOutgoingStates(expr_final, expr_edge_dot);
 		
 		/** construct a simulated call graph fsm */
 		Edge call_edge_foo = new Edge(new cgfsmId("foo"));
 		Edge call_edge_baz = new Edge(new cgfsmId("baz"));
+		Edge call_edge_main = new Edge(new cgfsmId("main"));
+		Edge call_edge_x = new Edge(new cgfsmId("x"));
+		Edge call_edge_y = new Edge(new cgfsmId("y"));
+		Edge call_edge_z = new Edge(new cgfsmId("z"));
 		
 		cgfsmState call_init = new cgfsmState(new cgfsmId(1), true, false, null);
-		cgfsmState call_state_1 = new cgfsmState(new cgfsmId(2), false, false, call_edge_foo);
-		cgfsmState call_final = new cgfsmState(new cgfsmId(3), false, true, call_edge_baz);
-		
-		
+		cgfsmState call_state_1 = new cgfsmState(new cgfsmId(2), false, true, call_edge_x);
+		cgfsmState call_state_2 = new cgfsmState(new cgfsmId(3), false, true, call_edge_y);
+		cgfsmState call_state_3 = new cgfsmState(new cgfsmId(4), false, true, call_edge_z);
+		cgfsmState call_state_4 = new cgfsmState(new cgfsmId(5), false, true, call_edge_main);
+		cgfsmState call_state_5 = new cgfsmState(new cgfsmId(6), false, true, call_edge_foo);
+		cgfsmState call_final = new cgfsmState(new cgfsmId(7), false, true, call_edge_baz);
+			
 		call.addStates(call_init);
 		call.addStates(call_state_1);
+		call.addStates(call_state_2);
+		call.addStates(call_state_3);
+		call.addStates(call_state_4);
+		call.addStates(call_state_5);
 		call.addStates(call_final);
 		
 		call.addInitState(call_init);
 		call.addFinalState(call_final);
 		
-		call_init.addOutgoingStates(call_state_1, call_edge_foo);
-		call_state_1.addOutgoingStates(call_final, call_edge_baz);
+		call_init.addOutgoingStates(call_state_1, call_edge_x);
+		call_init.addOutgoingStates(call_state_4, call_edge_main);
+		call_state_1.addOutgoingStates(call_state_2, call_edge_y);
+		call_state_1.addOutgoingStates(call_state_3, call_edge_z);
+		call_state_4.addOutgoingStates(call_state_5, call_edge_foo);
+		call_state_5.addOutgoingStates(call_final, call_edge_baz);
 		call_final.addOutgoingStates(call_final, call_edge_baz);
 		
 		//System.out.println("Hello world!");
