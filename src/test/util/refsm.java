39cd903db8f8ea1d2f71c984980aa78088410a58
xwangsd <xinyuwangsd@gmail.com>
2014-04-09 23:19:08 -0500

    rewrite find() method in refsm.java

--- src/test/util/refsm.java
+++ src/test/util/refsm.java
@@ -8,22 +8,27 @@ import java.util.Set;
 
 // refsm is the master fsm
 public class refsm extends fsm {
-
-	// all states in current fsm.
-
+	
+	// find() method returns a map that maps each state in refsm with a dot edge
+	// to a set of edges that must be followed to reach the one of the final states in refsm
 	public Map<State, Set<Edge>> find() {
 		Map<State, Set<Edge>> opts = new HashMap<State, Set<Edge>>();
 		for (State currState : states) {
 			// we want to optimize for the states with cycle
 			// so we only care about states that have (.*) edge
 			if (currState.hasOutgoingDotEdge()) {
+				// we do not regard final state as a qualified state
+				// and do not create an keyEdges set for the final state
+				// but we can change this freely by removing the following two LOC
 				if (currState.isFinalState())
 					continue;
-				opts.put(currState, new HashSet<Edge>());
+				
+				Set<Edge> keyEdges = new HashSet<Edge>();
+				opts.put(currState, keyEdges);
 				for (Object e : currState.outgoingStatesInv()) {
 					Edge eg = (Edge) e;
 					if (!eg.isDot())
-						opts.get(currState).add(eg);
+						keyEdges.add(eg);
 				}
 			}
 		}
