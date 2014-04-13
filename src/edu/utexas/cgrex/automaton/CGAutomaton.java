d61e572aed4d97f7063cd13c56980c864dff2d18
Xinyu Wang <xwang@cs.utexas.edu>
2014-04-13 13:27:43 -0500

    test scc

--- src/edu/utexas/cgrex/automaton/CGAutomaton.java
+++ src/edu/utexas/cgrex/automaton/CGAutomaton.java
@@ -4,75 +4,78 @@ import java.util.*;
 
 // cgfsm is the slave fsm
 public class CGAutomaton extends Automaton {
-	
+
 	// the first state in return value is the state in refsm
 	// the second state in return value is the state in cgfsm
-	// i.e., 
-	// Map <  State,   Map <   State,        Boolean   >  >
-	//          |                |              |
-	//      masterState      slaveState     true: create
+	// i.e.,
+	// Map < State, Map < State, Boolean > >
+	// | | |
+	// masterState slaveState true: create
 	// foreach masterState, "annotate" method annotates each slaveState
 	// to be true or false, denoting whether we should create the statePair
 	// in the intersection machine and whether we should go ahead
-	public Map<AutoState, Map<AutoState, Boolean>> annotate(Map<AutoState, Set<AutoEdge>> regExprOpts) {
-		Map<AutoState, Map<AutoState, Boolean>> opts = new HashMap<AutoState, Map<AutoState, Boolean>>();
+	public Map<AutoState, Map<AutoState, Boolean>> annotate(
+			Map<AutoState, Set<AutoEdge>> regExprOpts) {
+		Map<AutoState, Map<AutoState, Boolean>> opts = new HashMap<AutoState, 
+				Map<AutoState, Boolean>>();
 		for (AutoState stateInReg : regExprOpts.keySet()) {
 			Set<AutoEdge> keyEdges = regExprOpts.get(stateInReg);
 			opts.put(stateInReg, annotateOneMasterState(keyEdges));
 		}
 		return opts;
 	}
-	
+
 	// state: each state in cgfsm
 	// boolean: true -- this state is OK, and we can create and go ahead
-	// 			false -- the subgraph of this state does not have the key edge
-	// Map <  State,     Boolean  >
-	//          |           |
-	//      slaveState  true: create
-	protected Map<AutoState, Boolean> annotateOneMasterState(Set<AutoEdge> keyEdges) {
+	// false -- the subgraph of this state does not have the key edge
+	// Map < State, Boolean >
+	// | |
+	// slaveState true: create
+	protected Map<AutoState, Boolean> annotateOneMasterState(
+			Set<AutoEdge> keyEdges) {
 		Map<AutoState, Boolean> opts = new HashMap<AutoState, Boolean>();
 		for (AutoState s : initStates) {
 			annotateOneSlaveStateOneMasterState(s, keyEdges, opts);
 		}
 		return opts;
 	}
-	
+
 	// for each master state in regular expr fsm
 	// then for each slave state in call graph fsm
 	// annotate the slave state with true/false
-	// denoting whether the subgraph of the slave state has at least one key edge
+	// denoting whether the subgraph of the slave state has at least one key
+	// edge
 	// that leads to the final state of regular expr fsm
-	protected boolean annotateOneSlaveStateOneMasterState(AutoState currSlaveState, 
-			Set<AutoEdge> keyEdges, Map<AutoState, Boolean> opts) {
+	protected boolean annotateOneSlaveStateOneMasterState(
+			AutoState currSlaveState, Set<AutoEdge> keyEdges,
+			Map<AutoState, Boolean> opts) {
 		// termination conditions
 		// 1. if this slave state has been visited, return its boolean value
 		// this is just a speedup
-		if (opts.containsKey(currSlaveState)) 
+		if (opts.containsKey(currSlaveState))
 			return opts.get(currSlaveState);
-		// 2. if this slave state connects to some other state which will somehow
-		//    directly or indirectly connect to this slave state, we need to use
-		//    some method to handle this connected component case
-		//    lazily, we can set the boolean to be true every time we are trying
-		//    to annotate a state, in which case it is sound and can remove infinite
-		//    loop, but this might not be so accurate
-		//    another better way is to first find the connected components and then
-		//    handle this outside this method
-		
+		// 2. if this slave state connects to some other state which will
+		// somehow
+		// directly or indirectly connect to this slave state, we need to use
+		// some method to handle this connected component case
+		// lazily, we can set the boolean to be true every time we are trying
+		// to annotate a state, in which case it is sound and can remove
+		// infinite
+		// loop, but this might not be so accurate
+		// another better way is to first find the connected components and then
+		// handle this outside this method
+
 		// if this slave state has at least one outgoing edge (not cycle)
 		// do the recursive case
-		// we first check edges		
-		boolean edge_result = false, state_result = false; // might this be not sound? I guess it should be sound
-		/* 
-		 * this way also works but is silly, it is exponential
-		 * so we change to the way following this 
-		for (Object e : currSlaveState.outgoingStatesInv()) {
-			Edge eg = (Edge) e;
-			if (keyEdges.contains(eg)) {
-				edge_result = true;
-				break;
-			} 
-		}
-		*/
+		// we first check edges
+		boolean edge_result = false, state_result = false;
+		// might this be not sound? I guess it should be sound
+		/*
+		 * this way also works but is silly, it is exponential so we change to
+		 * the way following this for (Object e :
+		 * currSlaveState.outgoingStatesInv()) { Edge eg = (Edge) e; if
+		 * (keyEdges.contains(eg)) { edge_result = true; break; } }
+		 */
 		// this has only constant-complexity
 		for (AutoEdge e : keyEdges) {
 			if (currSlaveState.getOutgoingStatesInvKeySet().contains(e)) {
@@ -81,14 +84,16 @@ public class CGAutomaton extends Automaton {
 				break;
 			}
 		}
-		
+
 		// if we can in advance annotate this currSlaveState to be true
 		// we annotate it, which will be good if there is a cycle
 		// because when we check the annotations of the neighbors of this state
 		// and it happens that the neighbor's neighbors include this state
 		// then we can mark that neighbor as true
-		// but this does not help if there is a cycle and we cannot annotate true
-		// in which case we still need to handle the graph with connected components
+		// but this does not help if there is a cycle and we cannot annotate
+		// true
+		// in which case we still need to handle the graph with connected
+		// components
 		// but generally, we do not need this code and it still works
 		if (edge_result)
 			opts.put(currSlaveState, true);
@@ -96,12 +101,13 @@ public class CGAutomaton extends Automaton {
 		for (AutoState s : currSlaveState.getOutgoingStatesKeySet()) {
 			if (s.equals(currSlaveState))
 				continue; // eliminate infinite loop
-			state_result = state_result || annotateOneSlaveStateOneMasterState(s, keyEdges, opts);
+			state_result = state_result
+					|| annotateOneSlaveStateOneMasterState(s, keyEdges, opts);
 		}
 		// synthesize the result and annotate
 		boolean result = edge_result || state_result;
 		opts.put(currSlaveState, result);
-		
+
 		return result;
 	}
 }
--- src/edu/utexas/cgrex/test/TestInterAutomaton.java
+++ src/edu/utexas/cgrex/test/TestInterAutomaton.java
@@ -11,33 +11,32 @@ import edu.utexas.cgrex.automaton.RegAutoState;
 import edu.utexas.cgrex.automaton.RegAutomaton;
 import edu.utexas.cgrex.utils.GraphUtil;
 
-
 public class TestInterAutomaton {
 	public static void main(String[] args) {
 		CGAutomaton call = new CGAutomaton();
-		RegAutomaton expr = new RegAutomaton(); 
-		
+		RegAutomaton expr = new RegAutomaton();
+
 		/** construct a simulated regular expr fsm */
 		AutoEdge expr_edge_foo = new AutoEdge("foo");
-		AutoEdge expr_edge_baz = new AutoEdge("baz");	
+		AutoEdge expr_edge_baz = new AutoEdge("baz");
 		AutoEdge expr_edge_bar = new AutoEdge("bar");
 		AutoEdge expr_edge_dot = new AutoEdge("dot", true);
-		
+
 		RegAutoState expr_init = new RegAutoState(1, true, false);
 		RegAutoState expr_state_1 = new RegAutoState(2);
 		RegAutoState expr_state_2 = new RegAutoState(3);
 		RegAutoState expr_state_3 = new RegAutoState(4);
 		RegAutoState expr_final = new RegAutoState(5, false, true);
-		
+
 		expr.addStates(expr_init);
 		expr.addStates(expr_final);
 		expr.addStates(expr_state_1);
 		expr.addStates(expr_state_2);
 		expr.addStates(expr_state_3);
-		
+
 		expr.addInitState(expr_init);
 		expr.addFinalState(expr_final);
-		
+
 		expr_init.addOutgoingStates(expr_state_1, expr_edge_foo);
 		expr_init.addOutgoingStates(expr_state_2, expr_edge_bar);
 		expr_init.addOutgoingStates(expr_init, expr_edge_dot);
@@ -55,7 +54,7 @@ public class TestInterAutomaton {
 		expr_state_3.addIncomingStates(expr_state_2, expr_edge_baz);
 		expr_final.addOutgoingStates(expr_final, expr_edge_dot);
 		expr_final.addOutgoingStates(expr_final, expr_edge_dot);
-		
+
 		/** construct a simulated call graph fsm */
 		AutoEdge call_edge_foo = new AutoEdge("foo");
 		AutoEdge call_edge_baz = new AutoEdge("baz");
@@ -63,15 +62,17 @@ public class TestInterAutomaton {
 		AutoEdge call_edge_x = new AutoEdge("x");
 		AutoEdge call_edge_y = new AutoEdge("y");
 		AutoEdge call_edge_z = new AutoEdge("z");
-		
+
 		CGAutoState call_init = new CGAutoState(1, true, false, null);
 		CGAutoState call_state_1 = new CGAutoState(2, false, true, call_edge_x);
 		CGAutoState call_state_2 = new CGAutoState(3, false, true, call_edge_y);
 		CGAutoState call_state_3 = new CGAutoState(4, false, true, call_edge_z);
-		CGAutoState call_state_4 = new CGAutoState(5, false, true, call_edge_main);
-		CGAutoState call_state_5 = new CGAutoState(6, false, true, call_edge_foo);
+		CGAutoState call_state_4 = new CGAutoState(5, false, true,
+				call_edge_main);
+		CGAutoState call_state_5 = new CGAutoState(6, false, true,
+				call_edge_foo);
 		CGAutoState call_final = new CGAutoState(7, false, true, call_edge_baz);
-			
+
 		call.addStates(call_init);
 		call.addStates(call_state_1);
 		call.addStates(call_state_2);
@@ -79,10 +80,10 @@ public class TestInterAutomaton {
 		call.addStates(call_state_4);
 		call.addStates(call_state_5);
 		call.addStates(call_final);
-		
+
 		call.addInitState(call_init);
 		call.addFinalState(call_final);
-		
+
 		call_init.addOutgoingStates(call_state_1, call_edge_x);
 		call_init.addOutgoingStates(call_state_4, call_edge_main);
 		call_state_1.addOutgoingStates(call_state_2, call_edge_y);
@@ -90,45 +91,54 @@ public class TestInterAutomaton {
 		call_state_4.addOutgoingStates(call_state_5, call_edge_foo);
 		call_state_5.addOutgoingStates(call_final, call_edge_baz);
 		call_final.addOutgoingStates(call_final, call_edge_baz);
-		
-		//System.out.println("Hello world!");
+
+		// System.out.println("Hello world!");
 		expr.dump();
 		call.dump();
-		
+
 		InterAutomaton comb_without_opt = new InterAutomaton();
 		comb_without_opt.buildWithoutOpt(expr, call);
 		comb_without_opt.dump();
-		
+
 		Map<AutoState, Set<AutoEdge>> opts = expr.find();
 		System.out.println(opts);
-		Map<AutoState, Map<AutoState, Boolean>> annotations = call.annotate(opts);
+		Map<AutoState, Map<AutoState, Boolean>> annotations = call
+				.annotate(opts);
 		System.out.println(annotations);
-		
+
 		InterAutomaton comb_with_opt = new InterAutomaton();
 		comb_with_opt.buildWithOpt(expr, call);
 		comb_with_opt.dump();
-		
-		
+
 		/** test scc doAnalysis */
 		RegAutomaton scc = new RegAutomaton();
-		AutoEdge scc_edge = new AutoEdge("edge");
-		AutoEdge scc_back_edge = new AutoEdge("backEdge");
-		
+		AutoEdge scc_edge_1 = new AutoEdge("edge1");
+		AutoEdge scc_edge_2 = new AutoEdge("edge2");
+		AutoEdge scc_edge_3 = new AutoEdge("edge3");
+		AutoEdge scc_edge_4 = new AutoEdge("edge4");
+ 
 		RegAutoState scc_init = new RegAutoState(1, true, false);
 		RegAutoState scc_final = new RegAutoState(2, false, true);
-		
+		RegAutoState scc_state_1 = new RegAutoState(3);
+
 		scc.addStates(scc_init);
 		scc.addStates(scc_final);
-		
+		scc.addStates(scc_state_1);
+
 		scc.addInitState(scc_init);
 		scc.addFinalState(scc_final);
+
+		scc_init.addOutgoingStates(scc_final, scc_edge_1);
+		scc_init.addIncomingStates(scc_state_1, scc_edge_2);
+		scc_final.addOutgoingStates(scc_state_1, scc_edge_3);
+		scc_final.addIncomingStates(scc_state_1, scc_edge_4);
+		scc_final.addIncomingStates(scc_init, scc_edge_1);
+		scc_state_1.addOutgoingStates(scc_final, scc_edge_4);
+		scc_state_1.addOutgoingStates(scc_init, scc_edge_2);
+		scc_state_1.addIncomingStates(scc_final, scc_edge_3);
 		
-		scc_init.addOutgoingStates(scc_final, scc_edge);
-		scc_final.addIncomingStates(scc_init, scc_edge);
-		scc_init.addIncomingStates(scc_init, scc_back_edge);
-		scc_final.addOutgoingStates(scc_init, scc_back_edge);
 		scc.dump();
-				
+
 		Set roots = scc.getInitStates();
 		Map nodeToPreds = new HashMap<Object, Set<Object>>();
 		Map nodeToSuccs = new HashMap<Object, Set<Object>>();
@@ -140,8 +150,12 @@ public class TestInterAutomaton {
 		}
 		System.out.println(nodeToPreds);
 		System.out.println(nodeToSuccs);
-		
-		List<Set<Object>> sccList = GraphUtil.doAnalysis(roots, nodeToPreds, nodeToSuccs);
+
+		List<Set<Object>> sccList = GraphUtil.doAnalysis(roots, nodeToPreds,
+				nodeToSuccs);
 		System.out.println(sccList);
+		
+		InterAutomaton test_scc_with_opt = new InterAutomaton();
+		test_scc_with_opt.buildWithOpt(scc, call);
 	}
 }
