6d558ae14ab7c5131e0a8ee25634174f4a16a61b
Yu Feng <fengyu8299@gmail.com>
2014-04-25 10:08:51 -0500

    a test case for scc builder

--- src/edu/utexas/cgrex/utils/SCCBuilder.java
+++ src/edu/utexas/cgrex/utils/SCCBuilder.java
@@ -8,82 +8,81 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
 
-import soot.SootMethod;
-import soot.jimple.toolkits.callgraph.CallGraph;
-import soot.jimple.toolkits.callgraph.Edge;
-import soot.toolkits.graph.DirectedGraph;
+import edu.utexas.cgrex.automaton.AutoState;
+import edu.utexas.cgrex.automaton.Automaton;
 
 public class SCCBuilder {
-	  protected final List<List<Object>> componentList = new ArrayList<List<Object>>();
+	protected final List<List<Object>> componentList = new ArrayList<List<Object>>();
 
-	  protected int index = 0;
+	protected int index = 0;
 
-	  protected Map<Object,Integer> indexForNode, lowlinkForNode;
+	protected Map<Object, Integer> indexForNode, lowlinkForNode;
 
-	  protected Stack<Object> s;
+	protected Stack<Object> s;
 
-	  protected CallGraph g;
-	    
-	  /**
-	   *  @param g a graph for which we want to compute the strongly
-	   *           connected components. 
-	   *  @see DirectedGraph
-	   */
-	  public SCCBuilder(CallGraph g, Set roots)
-	  {
-	    this.g = g;
-	    s = new Stack<Object>();
-	    Set<Object> heads = roots;
+	protected Automaton g;
 
-	    indexForNode = new HashMap<Object, Integer>();
-	    lowlinkForNode = new HashMap<Object, Integer>();
+	/**
+	 * @param g: a automaton for which we want to compute the strongly connected
+	 * components.
+	 */
+	public SCCBuilder(Automaton g, Set roots) {
+		this.g = g;
+		s = new Stack<Object>();
+		Set<Object> heads = roots;
 
-	    for(Iterator<Object> headsIt = heads.iterator(); headsIt.hasNext(); ) {
-	      Object head = headsIt.next();
-	      if(!indexForNode.containsKey(head)) {
-	        recurse((SootMethod)head);
-	      }
-	    }
+		indexForNode = new HashMap<Object, Integer>();
+		lowlinkForNode = new HashMap<Object, Integer>();
 
-	    //free memory
-	    indexForNode = null;
-	    lowlinkForNode = null;
-	    s = null;
-	    g = null;
-	  }
+		for (Iterator<Object> headsIt = heads.iterator(); headsIt.hasNext();) {
+			Object head = headsIt.next();
+			if (!indexForNode.containsKey(head)) {
+				recurse((AutoState) head);
+			}
+		}
 
-	  protected void recurse(SootMethod v) {
-	    indexForNode.put(v, index);
-	    lowlinkForNode.put(v, index);
-	    index++;
-	    s.push(v);
-			
-		for (Iterator<Edge> cIt = g.edgesOutOf(v); cIt.hasNext();) {
-		  SootMethod succ = (SootMethod)cIt.next().getTgt();
+		// free memory
+		indexForNode = null;
+		lowlinkForNode = null;
+		s = null;
+		g = null;
+	}
 
-	      if(!indexForNode.containsKey(succ)) {
-	        recurse(succ);
-	        lowlinkForNode.put(v, Math.min(lowlinkForNode.get(v), lowlinkForNode.get(succ)));
-	      } else if(s.contains(succ)) {
-	        lowlinkForNode.put(v, Math.min(lowlinkForNode.get(v), indexForNode.get(succ)));
-	      }			
-	    }
-	    if(lowlinkForNode.get(v).intValue() == indexForNode.get(v).intValue()) {
-	      List<Object> scc = new ArrayList<Object>();
-	      Object v2;
-	      do {
-	        v2 = s.pop();
-	        scc.add(v2);
-	      }while(v!=v2);			
-	      componentList.add(scc);
-	    }
-	  }
+	protected void recurse(AutoState v) {
+		indexForNode.put(v, index);
+		lowlinkForNode.put(v, index);
+		index++;
+		s.push(v);
 
-	  /**
-	   *   @return the list of the strongly-connected components
-	   */
-	  public List<List<Object>> getComponents()
-	  {
-	    return componentList;
-	  }
+		for (AutoState succ : v.getOutgoingStatesKeySet()) {
+			if (!indexForNode.containsKey(succ)) {
+				recurse(succ);
+				lowlinkForNode.put(
+						v,
+						Math.min(lowlinkForNode.get(v),
+								lowlinkForNode.get(succ)));
+			} else if (s.contains(succ)) {
+				lowlinkForNode
+						.put(v,
+								Math.min(lowlinkForNode.get(v),
+										indexForNode.get(succ)));
+			}
+		}
+		if (lowlinkForNode.get(v).intValue() == indexForNode.get(v).intValue()) {
+			List<Object> scc = new ArrayList<Object>();
+			Object v2;
+			do {
+				v2 = s.pop();
+				scc.add(v2);
+			} while (v != v2);
+			componentList.add(scc);
+		}
+	}
+
+	/**
+	 * @return the list of the strongly-connected components
+	 */
+	public List<List<Object>> getComponents() {
+		return componentList;
+	}
 }
