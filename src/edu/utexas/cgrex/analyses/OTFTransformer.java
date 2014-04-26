3180521f9dcbaf12f4d7723c7f2f492dde71164b
Yu Feng <fengyu8299@gmail.com>
2014-04-26 16:14:22 -0500

    on-the-fly graph

---
+++ src/edu/utexas/cgrex/analyses/OTFTransformer.java
@@ -0,0 +1,107 @@
+package edu.utexas.cgrex.analyses;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import soot.G;
+import soot.Scene;
+import soot.SceneTransformer;
+import soot.SourceLocator;
+import soot.jimple.spark.builder.ContextInsensitiveBuilder;
+import soot.jimple.spark.pag.PAG;
+import soot.options.SparkOptions;
+
+
+/**
+ * Entry point of the whole analysis.
+ * 
+ * @author yufeng
+ * 
+ */
+public class OTFTransformer extends SceneTransformer {
+	
+	public boolean debug = true;
+	 
+    public OTFTransformer() {}
+    
+
+    protected void internalTransform( String phaseName, Map options )
+    {
+        Date startPrj = new Date();
+
+		HashMap<String, String> opt = new HashMap<String, String>(options);
+		opt.put("enabled","true");
+		opt.put("verbose","true");                  
+		opt.put("field-based","false");                
+		opt.put("on-fly-cg","true");                   
+		opt.put("set-impl","double");
+		opt.put("double-set-old","hybrid");         
+		opt.put("double-set-new","hybrid");		
+
+        SparkOptions opts = new SparkOptions( opt );
+        final String output_dir = SourceLocator.v().getOutputDir();
+        
+        // Build pointer assignment graph
+        ContextInsensitiveBuilder b = new ContextInsensitiveBuilder();
+
+//        if(debug) doGC();
+        Date startBuild = new Date();
+        final PAG pag = b.setup( opts );
+        b.build();
+        Date endBuild = new Date();
+        reportTime( "Pointer Assignment Graph", startBuild, endBuild );
+//        doGC();
+
+        // Build type masks
+        Date startTM = new Date();
+        pag.getTypeManager().makeTypeMask();
+        Date endTM = new Date();
+        reportTime( "Type masks", startTM, endTM );
+//        doGC();
+
+        if(debug) {
+            G.v().out.println( "VarNodes: "+pag.getVarNodeNumberer().size() );
+            G.v().out.println( "FieldRefNodes: "+pag.getFieldRefNodeNumberer().size() );
+            G.v().out.println( "AllocNodes: "+pag.getAllocNodeNumberer().size() );
+        }
+
+//        doGC();
+
+        // Propagate. This will run our actual points-to analysis.
+        Date startProp = new Date();
+        new OndemandWorklist( pag ).propagate();
+        Date endProp = new Date();
+        reportTime( "Propagation", startProp, endProp );
+
+//        doGC();
+        
+        if(debug) {
+            G.v().out.println( "[Spark] Number of reachable methods: "
+                    +Scene.v().getReachableMethods().size() );
+        }
+        
+		AutoPAG me = new AutoPAG(pag);
+		me.build();
+		
+        Date endPrj = new Date();
+        reportTime( "TOTAL project", startPrj, endPrj );
+		
+//		me.dump();
+
+    }
+    
+    protected static void reportTime( String desc, Date start, Date end ) {
+        long time = end.getTime()-start.getTime();
+        G.v().out.println( "[Spark] "+desc+" in "+time/1000+"."+(time/100)%10+" seconds." );
+    }
+    protected static void doGC() {
+        // Do 5 times because the garbage collector doesn't seem to always collect
+        // everything on the first try.
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+    }
+}
---
+++ src/edu/utexas/cgrex/analyses/OndemandWorklist.java
@@ -0,0 +1,267 @@
+package edu.utexas.cgrex.analyses;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import soot.G;
+import soot.MethodOrMethodContext;
+import soot.jimple.spark.pag.AllocDotField;
+import soot.jimple.spark.pag.AllocNode;
+import soot.jimple.spark.pag.FieldRefNode;
+import soot.jimple.spark.pag.Node;
+import soot.jimple.spark.pag.PAG;
+import soot.jimple.spark.pag.SparkField;
+import soot.jimple.spark.pag.VarNode;
+import soot.jimple.spark.sets.P2SetVisitor;
+import soot.jimple.spark.sets.PointsToSetInternal;
+import soot.jimple.spark.solver.OnFlyCallGraph;
+import soot.jimple.spark.solver.Propagator;
+import soot.jimple.spark.solver.TopoSorter;
+import soot.util.IdentityHashSet;
+import soot.util.queue.QueueReader;
+
+/**
+ * Propagating points-to set using xinyu's points-to analysis
+ * @author yufeng
+ *
+ */
+
+public final class OndemandWorklist extends Propagator {
+	
+	public boolean debug = true;
+    protected final Set<VarNode> varNodeWorkList = new TreeSet<VarNode>();
+
+    public OndemandWorklist( PAG pag ) { this.pag = pag; }
+    /** Actually does the propagation. */
+    public final void propagate() {
+        ofcg = pag.getOnFlyCallGraph();
+        new TopoSorter( pag, false ).sort();
+		for (Object object : pag.allocSources()) {
+			handleAllocNode((AllocNode) object);
+		}
+
+		do {
+			if (debug) {
+				G.v().out.println("Worklist has " + varNodeWorkList.size()
+						+ " nodes.");
+			}
+			while (!varNodeWorkList.isEmpty()) {
+				VarNode src = varNodeWorkList.iterator().next();
+				varNodeWorkList.remove(src);
+				handleVarNode(src);
+			}
+			if (debug) {
+				G.v().out.println("Now handling field references");
+			}
+			for (Object object : pag.storeSources()) {
+				final VarNode src = (VarNode) object;
+				Node[] targets = pag.storeLookup(src);
+				for (Node element0 : targets) {
+					final FieldRefNode target = (FieldRefNode) element0;
+					target.getBase().makeP2Set().forall(new P2SetVisitor() {
+						public final void visit(Node n) {
+							AllocDotField nDotF = pag.makeAllocDotField(
+									(AllocNode) n, target.getField());
+							nDotF.makeP2Set().addAll(src.getP2Set(), null);
+						}
+					});
+				}
+			}
+			HashSet<Object[]> edgesToPropagate = new HashSet<Object[]>();
+			for (Object object : pag.loadSources()) {
+				handleFieldRefNode((FieldRefNode) object, edgesToPropagate);
+			}
+			IdentityHashSet<PointsToSetInternal> nodesToFlush = new IdentityHashSet<PointsToSetInternal>();
+			for (Object[] pair : edgesToPropagate) {
+				PointsToSetInternal nDotF = (PointsToSetInternal) pair[0];
+				PointsToSetInternal newP2Set = nDotF.getNewSet();
+				VarNode loadTarget = (VarNode) pair[1];
+				if (loadTarget.makeP2Set().addAll(newP2Set, null)) {
+					varNodeWorkList.add(loadTarget);
+				}
+				nodesToFlush.add(nDotF);
+			}
+			for (PointsToSetInternal nDotF : nodesToFlush) {
+				nDotF.flushNew();
+			}
+		} while (!varNodeWorkList.isEmpty());
+	}
+
+    /* End of public methods. */
+    /* End of package methods. */
+
+    /** Propagates new points-to information of node src to all its
+     * successors. */
+	protected final boolean handleAllocNode(AllocNode src) {
+		boolean ret = false;
+		Node[] targets = pag.allocLookup(src);
+		for (Node element : targets) {
+			if (element.makeP2Set().add(src)) {
+				varNodeWorkList.add((VarNode) element);
+				ret = true;
+			}
+		}
+		return ret;
+	}
+    /** Propagates new points-to information of node src to all its
+     * successors. */
+	protected final boolean handleVarNode(final VarNode src) {
+		boolean ret = false;
+		boolean flush = true;
+
+		if (src.getReplacement() != src)
+			throw new RuntimeException("Got bad node " + src + " with rep "
+					+ src.getReplacement());
+
+		final PointsToSetInternal newP2Set = src.getP2Set().getNewSet();
+		if (newP2Set.isEmpty())
+			return false;
+
+		if (ofcg != null) {
+			QueueReader addedEdges = pag.edgeReader();
+			ofcg.updatedNode(src);
+			ofcg.build();
+
+			while (addedEdges.hasNext()) {
+				Node addedSrc = (Node) addedEdges.next();
+				Node addedTgt = (Node) addedEdges.next();
+				ret = true;
+				if (addedSrc instanceof VarNode) {
+					if (addedTgt instanceof VarNode) {
+						VarNode edgeSrc = (VarNode) addedSrc.getReplacement();
+						VarNode edgeTgt = (VarNode) addedTgt.getReplacement();
+
+						if (edgeTgt.makeP2Set()
+								.addAll(edgeSrc.getP2Set(), null)) {
+							varNodeWorkList.add(edgeTgt);
+							if (edgeTgt == src)
+								flush = false;
+						}
+					}
+				} else if (addedSrc instanceof AllocNode) {
+					AllocNode edgeSrc = (AllocNode) addedSrc;
+					VarNode edgeTgt = (VarNode) addedTgt.getReplacement();
+					if (edgeTgt.makeP2Set().add(edgeSrc)) {
+						varNodeWorkList.add(edgeTgt);
+						if (edgeTgt == src)
+							flush = false;
+					}
+				}
+			}
+		}
+
+		Node[] simpleTargets = pag.simpleLookup(src);
+		for (Node element : simpleTargets) {
+			if (element.makeP2Set().addAll(newP2Set, null)) {
+				varNodeWorkList.add((VarNode) element);
+				if (element == src)
+					flush = false;
+				ret = true;
+			}
+		}
+
+		Node[] storeTargets = pag.storeLookup(src);
+		for (Node element : storeTargets) {
+			final FieldRefNode fr = (FieldRefNode) element;
+			final SparkField f = fr.getField();
+			ret = fr.getBase().getP2Set().forall(new P2SetVisitor() {
+				public final void visit(Node n) {
+					AllocDotField nDotF = pag.makeAllocDotField((AllocNode) n,
+							f);
+					if (nDotF.makeP2Set().addAll(newP2Set, null)) {
+						returnValue = true;
+					}
+				}
+			})
+					| ret;
+		}
+
+		final HashSet<Node[]> storesToPropagate = new HashSet<Node[]>();
+		final HashSet<Node[]> loadsToPropagate = new HashSet<Node[]>();
+		for (final FieldRefNode fr : src.getAllFieldRefs()) {
+			final SparkField field = fr.getField();
+			final Node[] storeSources = pag.storeInvLookup(fr);
+			if (storeSources.length > 0) {
+				newP2Set.forall(new P2SetVisitor() {
+					public final void visit(Node n) {
+						AllocDotField nDotF = pag.makeAllocDotField(
+								(AllocNode) n, field);
+						for (Node element : storeSources) {
+							Node[] pair = { element, nDotF.getReplacement() };
+							storesToPropagate.add(pair);
+						}
+					}
+				});
+			}
+
+			final Node[] loadTargets = pag.loadLookup(fr);
+			if (loadTargets.length > 0) {
+				newP2Set.forall(new P2SetVisitor() {
+					public final void visit(Node n) {
+						AllocDotField nDotF = pag.makeAllocDotField(
+								(AllocNode) n, field);
+						if (nDotF != null) {
+							for (Node element : loadTargets) {
+								Node[] pair = { nDotF.getReplacement(), element };
+								loadsToPropagate.add(pair);
+							}
+						}
+					}
+				});
+			}
+		}
+		if (flush)
+			src.getP2Set().flushNew();
+		for (Node[] p : storesToPropagate) {
+			VarNode storeSource = (VarNode) p[0];
+			AllocDotField nDotF = (AllocDotField) p[1];
+			if (nDotF.makeP2Set().addAll(storeSource.getP2Set(), null)) {
+				ret = true;
+			}
+		}
+		for (Node[] p : loadsToPropagate) {
+			AllocDotField nDotF = (AllocDotField) p[0];
+			VarNode loadTarget = (VarNode) p[1];
+			if (loadTarget.makeP2Set().addAll(nDotF.getP2Set(), null)) {
+				varNodeWorkList.add(loadTarget);
+				ret = true;
+			}
+		}
+		return ret;
+	}
+
+    /** Propagates new points-to information of node src to all its
+     * successors. */
+	protected final void handleFieldRefNode(FieldRefNode src,
+			final HashSet<Object[]> edgesToPropagate) {
+		final Node[] loadTargets = pag.loadLookup(src);
+		if (loadTargets.length == 0)
+			return;
+		final SparkField field = src.getField();
+
+		src.getBase().getP2Set().forall(new P2SetVisitor() {
+
+			public final void visit(Node n) {
+				AllocDotField nDotF = pag.makeAllocDotField((AllocNode) n,
+						field);
+				if (nDotF != null) {
+					PointsToSetInternal p2Set = nDotF.getP2Set();
+					if (!p2Set.getNewSet().isEmpty()) {
+						for (Node element : loadTargets) {
+							Object[] pair = { p2Set, element };
+							edgesToPropagate.add(pair);
+						}
+					}
+				}
+			}
+		});
+	}
+    
+    protected PAG pag;
+    protected OnFlyCallGraph ofcg;
+}
+
+
+
