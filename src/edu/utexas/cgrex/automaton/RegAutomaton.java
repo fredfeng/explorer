94161d1ee36ee159b9af84d9148001fc29178ad8
xwangsd <xinyuwangsd@gmail.com>
2014-04-29 14:42:56 -0500

    change the annotation method in RegAutomaton.java, this version is more supportive for n-step look aheah

--- src/edu/utexas/cgrex/automaton/RegAutomaton.java
+++ src/edu/utexas/cgrex/automaton/RegAutomaton.java
@@ -11,8 +11,8 @@ import java.util.Set;
 
 // refsm is the master fsm
 public class RegAutomaton extends Automaton {
-	
-	//turn off debug before push to git
+
+	// turn off debug before push to git
 	private boolean debug = false;
 
 	Map<AutoState, Set<AutoEdge>> annotOneStep = new HashMap<AutoState, Set<AutoEdge>>();
@@ -56,132 +56,76 @@ public class RegAutomaton extends Automaton {
 	}
 
 	public void buildTwoStepAnnot() {
-		if(debug)
+		if (debug)
 			System.out.println("In RegAutomaton buildTwoStepAnnot method....");
 		LinkedList<AutoState> workList = new LinkedList<AutoState>();
 		Set<AutoState> annotated = new HashSet<AutoState>();
-
-		// annotate the last second states (the states right before the final
-		// states)
-		// for these states, we only fill the first step set
-		for (AutoState fs : finalStates) {
-			for (AutoState s : fs.getIncomingStatesKeySet()) {
-				if (s.isFinalState)
-					continue;
-
-				annotated.add(s);
-
-				AnnotTwoStepsWrapper keyEdges = new AnnotTwoStepsWrapper();
-				annotTwoSteps.put(s, keyEdges);
-				// annotate the first step
-				for (AutoEdge eg : s.getOutgoingStatesInvKeySet()) {
-					if (!eg.isDotEdge())
-						keyEdges.addFirstStep(eg);
-					else {
-						Set<AutoState> out = s.outgoingStatesInvLookup(eg);
-						for (AutoState os : out) {
-							if (!os.equals(s)) {
-								keyEdges.clearFirstStep();
-								break;
-							}
-						}
-					}
-				}
-				// add more elements in the worklist
-				for (AutoState prev : s.getIncomingStatesKeySet()) {
-					if (!annotated.contains(prev))
-						workList.add(prev);
-				}
-			}
-		}
-
-		// to be sure all states are considered, we can uncomment this
-		// however, this might degrade the performance
+		
+		// this is annotating the first step
 		for (AutoState s : states) {
-			workList.add(s);
-		}
+			// we want to optimize for the states with cycle
+			// so we only care about states that have (.*) edge
 
-		// a worklist algorithm to annotate all the other states
-		while (!workList.isEmpty()) {
-
-			AutoState head = workList.poll();
-
-			// eliminate infinite loops
-			if (annotated.contains(head))
+			// we do not need to annotate final states
+			if (s.isFinalState())
 				continue;
-
-			// dependency check
-			boolean restart = false;
-			for (AutoState next : head.getOutgoingStatesKeySet()) {
-				if (!next.equals(head) && !annotated.contains(next)) {
-					// if (!annotated.contains(next)) {
-					restart = true;
-					break;
-				}
-			}
-			if (restart) {
-				workList.add(head);
-				continue;
-			}
-
-			if(debug)
-				System.out.println("******Annotating state " + head);
-
-			if (head.isFinalState)
-				continue;
-
-			annotated.add(head);
 
 			AnnotTwoStepsWrapper keyEdges = new AnnotTwoStepsWrapper();
-			annotTwoSteps.put(head, keyEdges);
-			if(debug)
-				System.out.println("*****Before annotating first steps "
-					+ keyEdges.getFirstStep());
-			// System.out.println("*****Before annotating first steps " +
-			// keyEdges.getFirstStep());
+			annotTwoSteps.put(s, keyEdges);
+
 			// annotate the first step
-			for (AutoEdge eg : head.getOutgoingStatesInvKeySet()) {
+			for (AutoEdge eg : s.getOutgoingStatesInvKeySet()) {
 				if (!eg.isDotEdge())
 					keyEdges.addFirstStep(eg);
 				else {
-					Set<AutoState> out = head.outgoingStatesInvLookup(eg);
+					Set<AutoState> out = s.outgoingStatesInvLookup(eg);
 					for (AutoState os : out) {
-						if (!os.equals(head)) {
+						if (!os.equals(s)) {
 							keyEdges.clearFirstStep();
 							break;
 						}
 					}
 				}
 			}
-			if (debug) {
-				System.out.println("*****After annotating first steps: "
-						+ keyEdges.getFirstStep());
-				System.out.println("*****Before annotating second steps: "
-						+ keyEdges.getSecondStep());
-			}
-			// System.out.println("*****After annotating first steps: " +
-			// keyEdges.getFirstStep());
-			// System.out.println("*****Before annotating second steps: " +
-			// keyEdges.getSecondStep());
-			// annotate the second step
-			for (AutoState next : head.getOutgoingStatesKeySet()) {
-				if (next.equals(head))
-					continue;
-				assert (annotTwoSteps.containsKey(next));
-				// System.out.println("head: " + head);
-				// System.out.println("next: " + next);
-				keyEdges.addSecondStep(annotTwoSteps.get(next).getFirstStep());
-			}
-			if(debug)
-				System.out.println("*****After annotating second steps "
-					+ keyEdges.getSecondStep());
-			// add more elements in the worklist
-			for (AutoState prev : head.getIncomingStatesKeySet()) {
-				if (!annotated.contains(prev))
-					workList.add(prev);
-			}
+
 		}
 
+		// this is annotating the second step
+		long count = 0;
+		for (AutoState s : states) {
+			count++;
+			assert (count < 1000);
+
+			if (s.isFinalState())
+				continue;
+
+			if (annotated.contains(s))
+				continue;
+
+			annotated.add(s);
+
+			boolean jumpToNext = false;
+			for (AutoState fs : finalStates) {
+				if (s.getOutgoingStatesKeySet().contains(fs)) {
+					jumpToNext = true;
+					break;
+				}
+			}
+			if (jumpToNext)
+				continue;
+
+			assert (annotTwoSteps.containsKey(s));
+
+			AnnotTwoStepsWrapper keyEdges = annotTwoSteps.get(s);
+			for (AutoState next : s.getOutgoingStatesKeySet()) {
+				if (next.equals(s))
+					continue;
+
+				assert (annotTwoSteps.containsKey(next));
+
+				keyEdges.addSecondStep(annotTwoSteps.get(next).getFirstStep());
+			}
+		}
 	}
 
 	protected void test() {
