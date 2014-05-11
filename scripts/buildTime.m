5379cbc942e4d1aa2583cd3d346f1d8c310b8895
xwangsd <xinyuwangsd@gmail.com>
2014-05-10 23:16:01 -0500

    three figures

--- scripts/buildTime.m
+++ scripts/buildTime.m
@@ -1,7 +1,7 @@
 function buildTime()
-
+t = 22;
 cha = [2.24;2.25;8.194;8.445];
-otf = [20;20;20;20] - cha;
+otf = [t;t;t;t] - cha;
 mydata = [cha, otf];
 bar_h=bar(mydata, 0.5,'stack');
 mycolor=[1 0.6 1; 1 1 1; 1 1 1; 0.4 1 0.8];
@@ -14,10 +14,10 @@ end
 set(bar_child(1), 'CData',[0,0.2,0.3,0.4]);
 set(bar_child(1), 'CData',[0,1,0,1]);
 
-
+axis([0.5,4.5,0,t]);
 % lable setting
 xlabel('Benchmark','FontName','Times New Roman','FontSize',16);
-ylabel('Run time (second)','FontName','Times New Roman','FontSize',16);
+ylabel('Run time (seconds)','FontName','Times New Roman','FontSize',16);
 % legend setting
 legend('CHA','On-the-fly', 'Location','West');
 % axis setting
@@ -30,7 +30,9 @@ set(gca,'XTickLabel',{'soot-j','rhino','kawa-c','schroeder'});
 grid on;
 
 x = 0.96;
-text(x,20,'x','FontSize',25);
-text(1+x,20,'x','FontSize',25);
-text(2+x,20,'x','FontSize',25);
-text(3+x,20,'x','FontSize',25);
+text(x,t,'x','FontSize',25);
+text(1+x,t,'x','FontSize',25);
+text(2+x,t,'x','FontSize',25);
+text(3+x,t,'x','FontSize',25);
+str = 'inf';
+text(0.34,t,str,'FontName','Times New Roman','FontSize',16);
---
+++ scripts/perf.m
@@ -0,0 +1,42 @@
+function perf()
+figure();
+% x axis
+x = 0:500:2000;
+% On-the-fly call graph building time
+sootjCHA = 0.733;
+% CHA-based call graph building time
+sootjOTF = 233.594;
+% on-the-fly query run time
+sootjCHA1 = [sootjCHA,sootjCHA+[127.298, 257.398, 392.674, 513.794]];
+% CHA query run time
+sootjOTF1 = [sootjOTF,sootjOTF+[81.986, 159.422, 235.433, 310.181]];
+
+
+rhinoCHA = 0.587;
+rhinoOTF = 59.278;
+rhinoCHA1 = [rhinoCHA,rhinoCHA+[46.343, 92.933, 135.421, 175.725]];
+rhinoOTF1 = [rhinoOTF,rhinoOTF+[21.937, 40.945, 58.979, 77.425]];
+
+
+plot(x,sootjCHA1,'d-r','MarkerSize', 10, 'MarkerFaceColor', 'r', 'LineWidth', 1.01, 'MarkerFaceColor', 'w');
+hold on;
+plot(x,sootjOTF1,'s-b','MarkerSize', 10, 'MarkerFaceColor', 'b','LineWidth', 1.1, 'MarkerFaceColor', 'w');
+hold on;
+plot(x,rhinoCHA1,'+-g','MarkerSize', 10, 'MarkerFaceColor', 'g', 'LineWidth', 1.01, 'MarkerFaceColor', 'w');
+hold on;
+plot(x,rhinoOTF1,'*-m','MarkerSize', 10, 'MarkerFaceColor', 'm', 'LineWidth', 1.01, 'MarkerFaceColor', 'w');
+hold on;
+
+axis([-10,2010,0,600]);
+% lable setting
+xlabel('Number of queries','FontName','Times New Roman','FontSize',14);
+ylabel('Run time (seconds)','FontName','Times New Roman','FontSize',14);
+% legend setting
+legend('soot-j CHA', 'soot-j On-the-fly', 'rhino CHA', 'rhino On-the-fly', 'Location','NorthWest');
+% axis setting
+set(gca,'FontName','Times New Roman','FontSize',10);
+% title setting
+title('Runtime for queries','FontName','Times New Roman','FontSize',18);
+% gtext('21976.49');
+% gtext('209.46');
+grid on;
