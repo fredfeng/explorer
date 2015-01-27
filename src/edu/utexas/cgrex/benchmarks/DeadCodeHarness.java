36f23a0cfa03dfd3ec43e614aea96465cd1108ac
Yu Feng <fengyu8299@gmail.com>
2015-01-26 22:15:29 -0600

    harness to run android malware, need to include android.jar in classpath: https://github.com/Sable/android-platforms

---
+++ src/edu/utexas/cgrex/benchmarks/DeadCodeHarness.java
@@ -0,0 +1,75 @@
+package edu.utexas.cgrex.benchmarks;
+
+import soot.CompilationDeathException;
+import soot.PackManager;
+import soot.Transform;
+
+/**
+ * The harness for dead code detection.
+ * @author yufeng
+ *
+ */
+public class DeadCodeHarness {
+
+	public static int benchmarkSize = 10;
+	
+	//we will collect the running time at each interval.
+	public static int interval = 5;
+
+
+	// 0: interactive mode; 1: benchmark mode
+	public static int mode = 1;
+	
+	public static String queryLoc = "";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String targetLoc = args[0];
+		String targetMain = args[1];
+		queryLoc = args[2];
+		String alg = args[3];
+
+		System.out.println("benchmark----------" + targetLoc);
+		try {
+
+			if(alg.equals("cha"))
+				PackManager
+						.v()
+						.getPack("wjtp")
+						.add(new Transform("wjtp.regularPT",
+								new CHATransformer()));
+			else
+				PackManager
+				.v()
+				.getPack("wjtp")
+				.add(new Transform("wjtp.regularPT",
+						new OnTheFlyTransformer()));
+
+			soot.Main.v().run(
+					new String[] { "-W", "-process-dir", targetLoc,
+							"-src-prec", "java", "-allow-phantom-refs",
+							"-main-class", targetMain,
+							/*"-no-bodies-for-excluded", 
+							"-exclude", "java",
+							"-exclude", "javax", 
+							"-output-format", "none",*/
+							"-p", "jb", "use-original-names:true", });
+
+		} catch (CompilationDeathException e) {
+			e.printStackTrace();
+			if (e.getStatus() != CompilationDeathException.COMPILATION_SUCCEEDED)
+				throw e;
+			else
+				return;
+		}
+
+	}
+
+	public void run(String s) {
+
+	}
+
+}
